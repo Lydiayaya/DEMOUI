99de93958e246cd959173d18527a33e3cd2f729b
lydiayaya <2103207398@qq.com>
2023-04-27 11:05:10 +0800

    fix: eslint 类型检查

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App'
-const root = document.getElementById('root')!
-
+const root = document.getElementById('root') as Element | DocumentFragment
 ReactDOM.createRoot(root).render(<App />)
